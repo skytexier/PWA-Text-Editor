b6e006a739704d08aba4cff077482a44f2e04a90
Sky Texier <skyhamilton@gmail.com>
2022-12-30 21:43:24 -0800

    Webpack completed, configuring scripts
    Finished webpack plugins and manifest, working on scripts for install button

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,14 +15,35 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('PUT to database');
-  const todosDb = await openDB
+  //creating connection to DB
+  const jateDb = await openDB('jate', 1)
+  //creating transaction
+  const tx = jateDb.transaction('jate', 'readwrite');
+  // desired object store
+  const store = tx.objectStore('jate');
+  //request handling
+  const request = store.put({id: 1, value: content})
+  //result await
+  const result = await request
+  console.log(`Database updated with ${result.value}`)
   console.error('putDb not implemented');
 }
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET all content from database');
-  
-  console.error('getDb not implemented')
+  //creating connection to database and version we want to use
+  const jateDb = await openDB('jate', 1);
+  //creating new transaction and specifying the database/data privileges
+  const tx = jateDb.transaction('jate', 'readonly')
+  //open desired object store
+  const store = tx.objectStore('jate');
+  //GetAll to get data
+  const request = store.getAll();
+  const result = await request;
+  console.log('Get from database returned:', result.value)
+  ? err : console.error('getDb not implemented')
+  return result
+ 
 };
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const { InjectManifest, GenerateSW } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -20,11 +20,25 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Text Editor'
+        title: 'JATE Text Editor'
       }),
-      new MiniCssExtractPlugin(),
+      new WebpackPwaManifest({
+        name: "Just Another Text Editor",
+        short_name: "J.A.T.E",
+        description: "A text editor and note taker using webpacks",
+        publicPath: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ]
+      }),
       new InjectManifest({
-        swSrc: './src/js/sw.js'
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
       })
     ],
 
@@ -33,6 +47,17 @@ module.exports = () => {
         {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+          },
+        },
       },
       ],
     },
